aeeed59d48cb54fb8dfd34da4897cfce9d50d922
alexandresantoro2022 <111371207+alexandresantoro2022@users.noreply.github.com>
2025-08-09 07:24:58 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-# exit on error
+# Para o script se houver qualquer erro
 set -o errexit
 
+# Atualiza o pip e instala as dependências do requirements.txt
+pip install --upgrade pip
 pip install -r requirements.txt
 
+# Executa as migrações do banco de dados para garantir que ele está atualizado
 python -m flask db upgrade
+
+echo "Build finalizado com sucesso!"
+
